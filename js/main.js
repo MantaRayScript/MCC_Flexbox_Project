202b26fa6146e32ded3e905f6d0aba7d0fd5f546
Hagne-Studio <agas1993@gmail.com>
2023-10-04 17:27:00 +0100

    Adding function for the current year in the footer so the year will always be up to date

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const nav = document.querySelector(".nav");
 const navBtn = document.querySelector(".burger-btn");
 const allNavItems = document.querySelectorAll(".nav__item");
-const navBtnBars = document.querySelector('.burger-btn__bars')
-const allSections = document.querySelectorAll('.section')
-const footerYear = document.querySelector('.footer__year')
+const navBtnBars = document.querySelector(".burger-btn__bars");
+const allSections = document.querySelectorAll(".section");
+const footerYear = document.querySelector(".footer__year");
 
 const handleNav = () => {
 	nav.classList.toggle("nav--active");
@@ -20,8 +20,14 @@ const handleNavItemsAnimation = () => {
 	allNavItems.forEach((item) => {
 		item.classList.toggle("nav-items-animation");
 		item.style.animationDelay = "." + delayTime + "s";
-        delayTime++;
+		delayTime++;
 	});
 };
 
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
 navBtn.addEventListener("click", handleNav);
